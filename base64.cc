f6ab2116cd11da6db3f410f3478b941783ae6251
insufficientchocolate <tony84727@gmail.com>
2019-02-27 13:11:34 +0800

    fix base64
    the base64 outut is not the same with openssl version and without '='.
    But other implementation recognize it...

--- base64.cc
+++ base64.cc
@@ -1,5 +1,6 @@
 #include "base64.hpp"
 #include <sstream>
+
 static char characters[] =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz"
@@ -13,26 +14,43 @@ std::string Base64::encode(std::string input) {
   std::stringstream ss;
   const char* data = input.data();
   size_t s = input.size();
-  char left = 8;
+  char left = 0;
   for (size_t i = 0; i < s; i++) {
-    char value = 0;
-    // accumlate at least 6 bit
-    if (left < 6) {
-      left += 8;
-      continue;
-    }
+    left += 8;
+    /*
+    h                 e                 l
+    0 1 1 0 1 0 0 0 | 0 1 1 0 0 1 0 1 | 0 1 1 0 1 1 0 0
+    ----------|--------------|-------------|-----------|
+    loop and left:
+    ----------------| 0 + 8 => 8 (first)
+              |------------------------| (8 - 6) + 8 = 10 (second)
+                             |--------------------------| (10 - 6) + 8 = 12
+    (third)
+                                           |------------| 12 - 6 = 6 (forth)
+
+    */
     // have at least 6 bit
     while (left >= 6) {
       char value = 0;
+      char used = 0;
       if (left > 8) {
-        char use = left - 8;
-        value = data[i - 1] & !(255 << use);
-        left -= use;
+        const char fromLastByte = left - 8;   // 4
+        char remaining = (6 - fromLastByte);  // 2
+        char toMask = fromLastByte;           // 4
+        value = (data[i - 1] & ~(255 << toMask)) << remaining;
+        used += fromLastByte;
       }
-      char use = 8 - left;
-      value += data[i] >> use;
+      // left is equal to 6 bit, fetch 6 bit from current byte directly
+      if (left == 6) {
+        value = data[i] & ~(255 << 6);
+      } else {
+        value |= data[i] >> (8 - (6 - used));  // 2 + used
+      }
       ss << characters[value];
-      left -= use;
+      left -= 6;
+    }
+    if (i == s - 1) {
+      ss << characters[(data[i] & ~(255 << left)) << (6 - left)];
     }
   }
   return ss.str();
--- tests/base64_test.cc
+++ tests/base64_test.cc
@@ -3,6 +3,8 @@
 
 namespace {
 TEST(Base64, Encode) {
-  ASSERT_EQ("aGVsbG8gd29ybGQK", GithubClient::Base64::encode("helle world"));
+  ASSERT_EQ("aGVsbG8gd29ybGQ", GithubClient::Base64::encode("hello world"));
+  ASSERT_EQ("YSB2ZXJ5IHZlcnkgbG9uZyB0ZXh0A",
+            GithubClient::Base64::encode("a very very long text"));
 };
 };  // namespace
